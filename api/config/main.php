40dacddd1d1fe6a753168210f23a1288b15a4141
carl <carl@carldeMacBook-Pro.local>
2016-11-23 20:25:21 +0800

    微信API

---
+++ api/config/main.php
@@ -0,0 +1,52 @@
+<?php
+$params = array_merge(
+    require(__DIR__ . '/../../common/config/params.php'),
+    require(__DIR__ . '/../../common/config/params-local.php'),
+    require(__DIR__ . '/params.php'),
+    require(__DIR__ . '/params-local.php')
+);
+
+return [
+    'id' => 'app-api',
+    'basePath' => dirname(__DIR__),
+    'bootstrap' => ['log'],
+    'controllerNamespace' => 'api\controllers',
+    'defaultRoute' => 'site/index',
+    'components' => [
+         'user' => [
+             'identityClass' => 'common\models\User',
+             'enableAutoLogin' => true,
+         ],
+        'urlManager' => [
+            'enablePrettyUrl' => true,  //美化url==ture
+            'enableStrictParsing' => false,  //不启用严格解析
+            'showScriptName' => false,   //隐藏index.php
+            'rules' => [
+                '<module:\w+>/<controller:\w+>/<id:\d+>' => '<module>/<controller>/view',
+                '<controller:\w+>/<id:\d+>' => '<controller>/view',
+            ],
+        ],
+        'request'=>[
+            // Enable Yii Validate CSRF Token
+            'enableCsrfValidation' => false,
+        ],
+        'log' => [
+            'traceLevel' => YII_DEBUG ? 3 : 0,
+            'targets' => [
+                [
+                    'class' => 'yii\log\FileTarget',
+                    'levels' => ['error', 'warning'],
+                ],
+            ],
+        ],
+        'errorHandler' => [
+            'errorAction' => 'site/error',
+        ],
+        'assetManager' => [
+            'basePath' => '@webroot/api/web/assets',
+            'baseUrl' => '@web/api/web/assets'
+        ],
+    ],
+    'language' =>'zh-CN',
+    'params' => $params,
+];
